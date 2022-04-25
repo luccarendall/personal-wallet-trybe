b9775402b0df4329a33a0b3de1dbe828ba23afa2
Lucca Rendall <luccarendall1@gmail.com>
2022-04-25 19:16:30 -0300

    Corrigindo ES Lint

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -2,20 +2,19 @@
 // import { emailAction } from '../actions';
 
 const INICIAL_STATE = {
-    email: '',
-  };
-  
-  const user = (state = INICIAL_STATE, action) => {
-    switch (action.type) {
-    case 'EMAIL_ACTION':
-      return {
-        // ...state,
-        email: action.email,
-      };
-    default:
-      return state;
-    }
-  };
-  
-  export default user;
-  
+  email: '',
+};
+
+const user = (state = INICIAL_STATE, action) => {
+  switch (action.type) {
+  case 'EMAIL_ACTION':
+    return {
+      // ...state,
+      email: action.email,
+    };
+  default:
+    return state;
+  }
+};
+
+export default user;
--- src/services/FetchAPI.js
+++ src/services/FetchAPI.js
@@ -1,9 +1,8 @@
 const fetchAPI = () => {
-    const url = 'https://economia.awesomeapi.com.br/json/all';
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => console.log(data.value));
-  };
-  
-  export default fetchAPI;
-  
+  const url = 'https://economia.awesomeapi.com.br/json/all';
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => console.log(data.value));
+};
+
+export default fetchAPI;
