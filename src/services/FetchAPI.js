6ca3bd304326588527d85da001c770a5b70a8540
Lucca Rendall <luccarendall1@gmail.com>
2022-04-25 19:04:08 -0300

    Fetch

---
+++ src/services/FetchAPI.js
@@ -0,0 +1,9 @@
+const fetchAPI = () => {
+    const url = 'https://economia.awesomeapi.com.br/json/all';
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => console.log(data.value));
+  };
+  
+  export default fetchAPI;
+  
