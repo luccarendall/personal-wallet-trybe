04cc2e083c05454137957bf209c13cb13098644d
Lucca Rendall <luccarendall1@gmail.com>
2022-04-25 18:37:44 -0300

    Store

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,18 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+const INICIAL_STATE = {
+    payload: 0,
+  };
+  
+  const wallet = (state = INICIAL_STATE, action) => {
+    switch (action.type) {
+    case 'WALLET_ACTION':
+      return {
+        payload: action.payload,
+      };
+    default:
+      return state;
+    }
+  };
+  
+  export default wallet;
+  
