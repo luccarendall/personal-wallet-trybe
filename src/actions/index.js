a91f53f7a8a00fcfd154748f046d7db885f4ce03
Lucca Rendall <luccarendall1@gmail.com>
2022-04-25 18:34:06 -0300

    Reiniciando projeto. O primeiro deu um erro estranho

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,12 @@
-// Coloque aqui suas actions
+export const EMAIL_ACTION = 'EMAIL_ACTION';
+export const WALLET_ACTION = 'WALLET_ACTION';
+
+export const emailAction = (email) => ({
+  type: EMAIL_ACTION,
+  email,
+});
+
+export const walletAction = (payload) => ({
+  type: WALLET_ACTION,
+  payload,
+});
