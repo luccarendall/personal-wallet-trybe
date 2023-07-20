554df49caea1cf91b24add1a48b2e26b50f8cb5a
LuccaRendall <luccarendall2@gmail.com>
2023-07-20 10:05:12 -0300

    style: componete header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import './style/header.css';
 
 class Header extends Component {
   componentDidMount() {
@@ -15,35 +16,43 @@ class Header extends Component {
     const username = userEmail.split('@')[0];
 
     return (
-      <>
-        <h2>
+      <div className="header-container">
+        <h2 className="header-title">
           Olá,
           {' '}
           {username}
         </h2>
         {/* Email da pessoa usuária que fez login */}
-        <p data-testid="email-field">
+        <p className="header-email" data-testid="email-field">
           {userEmail}
         </p>
 
         {/* Despesa total gerada pela lista de gastos */}
-        Gastos totais:
-        <p data-testid="total-field">
-          {totalExpenses.length < 1
-            ? 0
-            : totalExpenses
-              .map(
-                (obj) => Number(obj.value) * Number(obj.exchangeRates[obj.currency].ask),
-              )
-              .reduce((a, b) => a + b)
-              .toFixed(2)}
-        </p>
+        <div className="header-total">
+          <div>
+            <p>
+              Gastos totais:
+            </p>
+          </div>
+          <p className="total-expenses" data-testid="total-field">
+            {totalExpenses.length < 1
+              ? 0
+              : totalExpenses
+                .map(
+                  (
+                    obj,
+                  ) => Number(obj.value) * Number(obj.exchangeRates[obj.currency].ask),
+                )
+                .reduce((a, b) => a + b)
+                .toFixed(2)}
+          </p>
+        </div>
 
-        {/* Câmbio está sendo utilizado, que será neste caso será 'BRL' */}
-        <p data-testid="header-currency-field">
+        {/* Câmbio que está sendo utilizado, que será neste caso será 'BRL' */}
+        <p className="header-currency" data-testid="header-currency-field">
           BRL
         </p>
-      </>
+      </div>
     );
   }
 }
